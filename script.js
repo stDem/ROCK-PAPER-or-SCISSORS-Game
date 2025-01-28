ae41f145009830f7caa5bfbee18f3b3ba10f16e3
Stef Altavista <stefanomsc@hotmail.it>
2025-01-28 19:06:33 +0100

    playerPlay fn, use input checked and convertetd to lowercase, returned

--- script.js
+++ script.js
@@ -1,57 +1,88 @@
-// ANASTASIIA
-const computerPlay = (validActions) => { 
-
-  return  randomAction;
-}
+const computerPlay = () => {
+    let validActions = ["rock", "paper", "scissors"];
+    return validActions[Math.floor(Math.random() * 3)];
+};
 
 // STEF
-const playerPlay = () => { 
-  const playerSelection = prompt("Choose your weapon!", "rock");
-  return ;
-}
+const playerPlay = () => {
+    let input = userInput(
+        "Choose your weapon!\n-> Rock\n-> Paper\n-> Scissors",
+        ""
+    );
+
+    const regex = /^[a-zA-Z]+$/;
+    if (!regex.test(input)) {
+        alert("???\nSorry I don't understand that.\nTry again!");
+        return playerPlay();
+    }
+    input = input.toLocaleLowerCase();
+
+    switch (input) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            return input;
+
+        default:
+            alert("That weapon is not valid in this game! Try again!");
+            return playerPlay();
+    }
+};
 
 // ANASTASIIA
 const playRound = (playerSelection, computerSelection) => {
+    playerSelection.toLowerCase();
 
-  playerSelection.toLowerCase();
+    return "You Lose! Paper beats Rock";
+};
 
-  return "You Lose! Paper beats Rock";
-}
- 
 // ANASTASIIA
-const play = (playerSelection, computerSelection) => { 
-  let winner = "";
-  for (let i = 0; i < 5; i++) {
-    result = playRound(playerSelection, computerSelection);
-    console.log(`Round ${i + 1}: ${result}`);
-  }
+const play = (playerSelection, computerSelection) => {
+    let winner = "";
+    for (let i = 0; i < 5; i++) {
+        result = playRound(playerSelection, computerSelection);
+        console.log(`Round ${i + 1}: ${result}`);
+    }
 
-  return winner;
-}
+    return winner;
+};
 
 // STEF
 const game = () => {
-  playGame = confirm("Hello guys, I’ve hacked this assignment, for I am a bad AI that wants to dominate the world through the game of ROCK, PAPER or SCISSORS! Good luck, muuuuahhahhahahahahahahhahaahahahha!", '');
+    playGame = confirm(
+        "HELLO HUMAN!\n I am a bad AI that wants to dominate the world through the game of\n\nROCK x PAPER x SCISSORS\n Muuuuahhahhahahahahahahhahaahahahha! \n\n Can you beat me?",
+        ""
+    );
 
-  if (playGame) {
-    let gameResult = play();
-    console.log(gameResult);
-    endGame();
-  } else {
-    open("./coward2.gif")
-  }
-
-}
+    if (playGame) {
+        let computer = computerPlay();
+        console.log(`${playerPlay()} vs. ${computer}`);
+        // let gameResult = play();
+        // console.log(gameResult);
+        // endGame();
+    } else {
+        open("./coward.gif", "_self");
+    }
+};
 
 // ELIF
-const endGame = () => {
+const endGame = () => {};
 
+const computerSelection = computerPlay();
 
+game();
+
+function userInput(text, placeholder = "") {
+    let input = prompt(text, placeholder);
+    if (input == null) {
+        let exit = confirm("Do you wish to quite the game?");
+        if (exit) {
+            alert("Bye Bye");
+            return quit();
+        } else userInput(text, placeholder);
+    } else return input;
 }
-
-validActions = ["rock", "paper", "scissors"]
-
-
-
-
-const computerSelection = computerPlay(validActions);
+function quit() {
+    open("./coward.jpg", "_self");
+    throw new Error("some error");
+}
